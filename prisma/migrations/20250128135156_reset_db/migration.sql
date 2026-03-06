da72bc57707627e100969fba956ded4d5bf5159b
SeanbenTinaPaul <t.mutational@gmail.com>
2026-03-07 02:28:37 +0700

    feat: Implement K6 experiment replication tools including database reset, seeding scripts, and configuration files.

--- prisma/migrations/20250128135156_reset_db/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "amount" SET DEFAULT 0,
-ALTER COLUMN "currency" SET DEFAULT 'thb',
-ALTER COLUMN "paymentId" SET DEFAULT '',
-ALTER COLUMN "status" SET DEFAULT '';
